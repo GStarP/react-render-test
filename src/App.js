ea46b70c33c96c2321a9fff8426954d4c5a7254e
GStarP <1700066935@qq.com>
2023-03-10 17:14:26 +0800

    feat: useMemo

--- src/App.js
+++ src/App.js
@@ -1,8 +1,48 @@
+/**
+ * @branch use-memo
+ * @target when to use useMemo?
+ */
+
+/**
+ * useMemo is the same as useCallback, more specifically, useCallback is a syntax sugar for useMemo!
+ * So, useMemo also works with React.memo, and component should depends on a parent component generated object.
+ */
+
+import { memo, useMemo, useState } from "react";
+
+const MemoCalculator = memo((props) => {
+  console.log("MemoCalculator render", props.id);
+  return (
+    <div>
+      <div>calculator {props.id}</div>
+      <button onClick={props.operation.inc}>Inc</button>
+      <button onClick={props.operation.dec}>Dec</button>
+    </div>
+  );
+});
+
 function App() {
+  const [count, setCount] = useState(0);
+
+  const operation = {
+    inc: () => setCount((val) => val + 1),
+    dec: () => setCount((val) => val - 1),
+  };
+
+  const memoOperaion = useMemo(() => {
+    return {
+      inc: () => setCount((val) => val + 1),
+      dec: () => setCount((val) => val - 1),
+    };
+  }, []);
+
   return (
     <div className="App">
-      <h1>Study How React Render</h1>
-      <h2>switch branch to continue...</h2>
+      <h2>{count}</h2>
+      {/* re-render */}
+      <MemoCalculator operation={operation} id="1"></MemoCalculator>
+      {/* not re-render */}
+      <MemoCalculator operation={memoOperaion} id="2"></MemoCalculator>
     </div>
   );
 }
